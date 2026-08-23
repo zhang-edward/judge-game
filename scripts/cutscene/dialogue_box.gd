ee0e21f607e0a00ea741266b9a9064449dd31133
Edward Zhang <edwardz123@gmail.com>
2026-08-23 00:45:22 -0400

    Add sfx for draggables and dialogue box

--- scripts/cutscene/dialogue_box.gd
+++ scripts/cutscene/dialogue_box.gd
@@ -4,11 +4,23 @@ extends PanelContainer
 signal reveal_finished
 
 const CHARS_PER_SECOND := 45.0
+const DIALOGUE_SOUND := preload("res://assets/sfx/dialogue.wav")
 
 @onready var label: Label = %Label
 
 var _revealing := false
 var _tween: Tween
+var _sfx: AudioStreamPlayer
+
+func _ready() -> void:
+	_sfx = AudioStreamPlayer.new()
+	add_child(_sfx)
+	_sfx.stream = DIALOGUE_SOUND
+	_sfx.finished.connect(_on_sfx_finished)
+
+func _on_sfx_finished() -> void:
+	if _revealing:
+		_sfx.play()
 
 func play(text: String) -> void:
 	if _tween != null:
@@ -16,6 +28,7 @@ func play(text: String) -> void:
 	label.text = text
 	label.visible_characters = 0
 	_revealing = true
+	_sfx.play()
 	_tween = create_tween()
 	_tween.tween_property(label, "visible_characters", text.length(), text.length() / CHARS_PER_SECOND)
 	_tween.finished.connect(_on_reveal_done)
@@ -33,4 +46,5 @@ func finish_reveal() -> void:
 
 func _on_reveal_done() -> void:
 	_revealing = false
+	_sfx.stop()
 	reveal_finished.emit()
